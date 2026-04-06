2af72ef169fd73610397dade1d25257e194d8e3c
MustafaDevs <mustafaahmadfaqiryar@gmail.com>
2026-04-05 22:58:29 -0400

    Add BattleGame.java

---
+++ src/BattleGame.java
@@ -0,0 +1,6 @@
+public class BattleGame {
+    public static void main(String[] args) throws Exception {
+        Spell fireball = new Spell("Fireball", 25, 100, 0.5);
+        System.out.println(fireball);
+    }
+}
